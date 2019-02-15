ef07fc5242646150c88236e07c11b814e2f6a641
Hahnsang Kim <hahnsang@Hahnsangs-MacBook-Pro.local>
2019-02-15 15:36:03 -0800

    feat: reroute the header links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     renderContent() {
@@ -9,7 +10,7 @@ class Header extends Component {
             case false:
                 return <li><a href="/auth/google">Login With Google</a></li>;
             default:
-                return <li><a>Logout</a></li>;
+                return <li><a href="/api/logout">Logout</a></li>;
         }
     }
     render() {
@@ -17,7 +18,10 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                <a className="left brand-logo">Logo</a>
+                <Link to={this.props.auth ? '/userveys': '/'} 
+                className="left brand-logo"
+                >Emaily
+                </Link>
                 <ul className="right">
                     {this.renderContent()}
                 </ul>
