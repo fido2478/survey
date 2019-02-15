d98ac6d7909e8de56251c77fe4bc5223af17e5b9
Hahnsang Kim <hahnsang@Hahnsangs-MacBook-Pro.local>
2019-02-14 20:27:15 -0800

    style: install materializecss

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,18 @@
 // All Redux setup is here!
 // All data setup is here!
+// css can be imported in index.js or App.js
+import materializeCSS from 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
 // Redux store
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
     <Provider store={store}><App /></Provider>, 
