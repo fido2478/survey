a503f65ed7a2ad29b5e630f96d87049e563282f2
Hahnsang Kim <hahnsang@Hahnsangs-MacBook-Pro.local>
2019-02-15 13:43:57 -0800

    feat: add function: menu in header changed depending on the user's loggin status

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return ;
+            case false:
+                return <li><a href="/auth/google">Login With Google</a></li>;
+            default:
+                return <li><a>Logout</a></li>;
+        }
+    }
     render() {
         console.log(this.props)
         return (
             <nav>
                 <div className="nav-wrapper">
-                <a href="#" className="left brand-logo">Logo</a>
-                <ul id="nav-mobile" className="right">
-                    <li><a href="#">Login with Google</a></li>
+                <a className="left brand-logo">Logo</a>
+                <ul className="right">
+                    {this.renderContent()}
                 </ul>
                 </div>
             </nav>
